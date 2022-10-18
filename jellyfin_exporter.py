6c4a30a87e2b5dadd21911df82f7c1470e2c0e40
drkhsh <me@drkhsh.at>
2022-10-19 00:50:04 +0200

    Add jellyfin_exporter and requirements

---
+++ jellyfin_exporter.py
@@ -0,0 +1,80 @@
+import os
+import sys
+import time
+import threading
+import logging
+
+import requests
+from prometheus_client import start_http_server
+from prometheus_client.core import GaugeMetricFamily, REGISTRY
+
+PORT = os.getenv('JELLYFIN_EXPORTER_PORT', 9027)
+API_BASEURL = os.getenv('JELLYFIN_BASEURL', '')
+API_KEY = os.getenv('JELLYFIN_APIKEY', '')
+
+logging.basicConfig(format = '%(asctime)s %(levelname)s %(message)s',
+                    level = logging.INFO,
+                    datefmt = '%Y-%m-%d %H:%M:%S')
+
+if API_BASEURL == '': 
+    logging.error("JELLYFIN_BASEURL environment variable is required.")
+    sys.exit(1)
+if API_KEY == '':
+    logging.error("JELLYFIN_APIKEY environment variable is required.")
+    sys.exit(1)
+
+logging.info("Starting jellyfin_exporter for '%s' on port: %d", str(API_BASEURL), PORT)
+
+def request_api(action):
+    url = '{}{}?api_key={}'.format(API_BASEURL, action, API_KEY)
+    start = time.time()
+    data = requests.get(url).json()
+    elapsed = time.time() - start
+    logging.info("Request to %s returned in %s",
+                url, elapsed)
+    return data
+
+class JellyfinCollector(object):
+    def collect(self):
+        try:
+            sessions_data = request_api('/Sessions')
+
+            sessions_count = 0
+            streams_count = 0
+            sessions = GaugeMetricFamily(
+                'jellyfin_active_users', 'Jellyfin active user sessions', labels=['user', 'client', 'device_name', 'jellyfin_instance'])
+            for user in sessions_data:
+                if user['IsActive'] == True:
+                    sessions.add_metric([user['UserName'], user['Client'], user['DeviceName'], API_BASEURL], 1)
+                    sessions_count += 1
+
+                if 'NowPlayingItem' in user:
+                    streams_count += 1
+            yield sessions
+
+            active = GaugeMetricFamily(
+                'jellyfin_active_users_count', 'Jellyfin active user count', labels=['jellyfin_instance'])
+            active.add_metric([API_BASEURL], sessions_count)
+            yield active
+
+            streams = GaugeMetricFamily(
+                'jellyfin_active_streams_count', 'Jellyfin active streams count', labels=['jellyfin_instance'])
+            streams.add_metric([API_BASEURL], streams_count)
+            yield streams
+
+            items_counts_data = request_api('/Items/Counts')
+
+            items_counts = GaugeMetricFamily(
+                'jellyfin_item_counts', 'Jellyfin items counts', labels=['type', 'jellyfin_instance'])
+            for metric, val in items_counts_data.items():
+                items_counts.add_metric([metric, API_BASEURL], val)
+            yield items_counts
+
+        except Exception as ex:
+            logging.error('Error getting metrics: %s', ex)
+
+REGISTRY.register(JellyfinCollector())
+start_http_server(PORT)
+
+e = threading.Event()
+e.wait()
